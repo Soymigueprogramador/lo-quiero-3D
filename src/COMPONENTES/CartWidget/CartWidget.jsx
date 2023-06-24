c4fcf5aac42025c91b864f2ec51cbc40b97948ff
Miguel Salazar <yobebe@hotmail.com.ar>
2023-06-23 21:01:35 -0300

    actualizando el repo, perdon la demora

--- src/COMPONENTES/CartWidget/CartWidget.jsx
+++ src/COMPONENTES/CartWidget/CartWidget.jsx
@@ -1,7 +1,7 @@
 import "./CartWidget.css"
 
 const CartWidget = () => {
-  const carrito = "https://cdn-icons-png.flaticon.com/512/107/107831.png";
+  const carrito = "C:\Users\yobeb\OneDrive\Escritorio\lo-quiero-3d-2\lo-quiero-3d-2\src\assets\img";
     return (
     <div>
         <img className="carrito" src="{carrito}" alt="este es un carrito de compras con 5 productos" />
